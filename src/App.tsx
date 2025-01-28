dd5fbdaa548af8f4a4b6c2f5770fa44461476f13
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2025-01-28 19:02:06 +0100

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import viteLogo from "/vite.svg";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+
+function App() {
+
+  return (
+    <Routes>
+      <Route />
+      <Route />
+      <Route />
+      <Route />
+      <Route />
+    </Routes>
+  );
+}
+
+export default App;
