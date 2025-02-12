80f945bbc49a171b17da657f9143c698b646d4aa
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2025-02-12 20:45:36 +0100

    Refactor Loading component styles and reduce animation duration for improved user experience

--- src/components/loading/Loading.tsx
+++ src/components/loading/Loading.tsx
@@ -6,9 +6,9 @@ export const Loading: React.FC<Props> = ({ size }) => {
   return (
     <>
       <div
-        className={`animate-bounce absolute bottom-0 left-1/2 transform -translate-x-1/2`}
+        className={`animate-bounce text-center w-40 absolute bottom-0 left-1/2 transform -translate-x-1/2`}
       >
-        <p>It is free server sorry for long waiting 😥</p>
+        <p className="w-fit">It is free server sorry for long waiting 😥</p>
         <div
           className={`animate-spin rounded-full  border-t-4 border-t-[var(--color-primary)] border-b-4 border-b-[var(--color-secondary)]`}
           style={{ width: size, height: size }}
