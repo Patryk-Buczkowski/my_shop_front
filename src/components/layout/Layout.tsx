1c8d9df2791b681156f89a5e0224aea7ae184ec8
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2025-02-15 20:23:26 +0100

    Fix import order for Header component in Layout for consistency

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import { Header } from "../header/Header";
 import { Footer } from "../footer/Footer";
+import { Header } from "../header/Header";
 
 export const Layout: React.FC = () => {
   return (
