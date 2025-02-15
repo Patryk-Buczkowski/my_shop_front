d2bc643fc59bcfa40a8d902423ab14b37f149a78
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2025-02-15 20:51:31 +0100

    Fix import paths for Header and Footer components in Layout for consistency

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import { Footer } from "../footer";
-import { Header } from "../header";
+import { Footer } from "../Footer/Footer";
+import { Header } from "../Header/Header";
 
 export const Layout: React.FC = () => {
   return (
