8d760755cea286378f1a06c23bd8af602c3eb1a7
Patryk-Buczkowski <patryk.buczkowski@outlook.com>
2025-03-09 14:32:51 +0100

    Update ButtonMinus component to use consistent product identifier; change subtract method to use _id instead of id

--- src/components/buttonMinus/ButtonMinus.tsx
+++ src/components/buttonMinus/ButtonMinus.tsx
@@ -9,7 +9,7 @@ export const ButtonMinus: React.FC<Props> = ({product, subtract}) => {
   return (
     <>
       <div
-        onClick={() => subtract(product.id)}
+        onClick={() => subtract(product._id)}
         className="w-5 h-5 flex justify-items-center justify-center align-baseline rounded-full border-2 border-[var(--color-primary)]"
       >
         <Minus color="var(--color-primary)" size={15} />
